c0e301400b5ba9b35f4dd092d3c50cf0d90612cc
Thiago Gregorio <thiago.gregorio@4win.local>
2024-03-08 11:15:45 -0300

    alteração

--- PAGE-WEB-SABESP/cypress/e2e/login.cy.js
+++ PAGE-WEB-SABESP/cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe('Acesso a pagina Sabesp de HOMO', () => {
   it('Acessar a pagina da Sabesp de Homologação', () => {
-    cy.visit('/')
+    cy.visit('https://www.sabesp.com.br/')
     cy.get('#fechaPop > .mat-button-wrapper > .mat-icon').click({force:true})
     cy.get('.botao-aceitar').click()
     cy.get('.cpf').type('739.703.398-98')
