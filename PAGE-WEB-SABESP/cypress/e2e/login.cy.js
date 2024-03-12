234d4ef8c391efbe0af72be5d6f3b2c89c314a16
TGregorio11 <thiago.gregorio11@gmail.com>
2024-03-12 08:18:34 -0300

    mod

--- PAGE-WEB-SABESP/cypress/e2e/login.cy.js
+++ PAGE-WEB-SABESP/cypress/e2e/login.cy.js
@@ -1,9 +1,17 @@
+
+require ('cypress-xpath'); 
 describe('Acesso a pagina Sabesp de HOMO', () => {
   it('Acessar a pagina da Sabesp de Homologação', () => {
-    cy.visit('https://www.sabesp.com.br/')
-    cy.get('#fechaPop > .mat-button-wrapper > .mat-icon').click({force:true})
+    cy.visit('https://agenciavirtual.sabesp.com.br') , // { timeout: 60000}) 
+    cy.xpath('//*[@id="fechaPop"]/span[1]/mat-icon').click()
     cy.get('.botao-aceitar').click()
-    cy.get('.cpf').type('739.703.398-98')
-    cy.get('.senha').type('@Sabesp4Win')
+    cy.get('#cpf').type('739.703.398-98')
+    cy.get('#senha').type('@Sabesp4Win')
+    cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click()
+    cy.get('iframe[src*=recaptcha]')
+    .its('0.contentDocument')
+    .should(d => d.getElementById('recaptcha-token').click())
+    cy.get('[style="z-index: 2000000000; position: relative; width: 400px; height: 580px;"] > iframe').click()
+
   })
 })
